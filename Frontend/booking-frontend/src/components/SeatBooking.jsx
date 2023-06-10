6d5bf2314b0f03323ddcf8071f5fd938a982696b
Vicky Kumar <vickykumar@Vickys-MacBook-Air.local>
2023-06-10 11:32:00 +0530

    frontend ready

--- Frontend/booking-frontend/src/components/SeatBooking.jsx
+++ Frontend/booking-frontend/src/components/SeatBooking.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function SeatBooking() {
   const totalSeats = 80;
@@ -7,40 +7,77 @@ function SeatBooking() {
 
   const [seats, setSeats] = useState(new Array(totalSeats).fill(false));
   const [bookedSeats, setBookedSeats] = useState([]);
-  const [seatCount, setSeatCount] = useState();
-  const [Alert, setAlert] = useState("");
+  const [seatCount, setSeatCount] = useState("");
+  const [alert, setAlert] = useState("");
+
+  useEffect(() => {
+    fetchBookedSeats();
+  }, []);
+
+  const fetchBookedSeats = () => {
+    fetch("http://localhost:8080/booked-seats")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        if (data.bookedSeats && data.bookedSeats.length > 0) {
+          const newSeats = [...seats];
+          data.bookedSeats.forEach((seatNumber) => {
+            const seatIndex = getSeatIndex(seatNumber);
+            if (seatIndex !== -1) {
+              newSeats[seatIndex] = true;
+            }
+          });
+          setSeats(newSeats);
+          setBookedSeats(data.bookedSeats);
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
 
   const handleSeatBooking = () => {
-    let bookedSeats = []; // Store the seat numbers that were booked
-
-    for (let i = 0; i <= totalSeats - seatCount; i++) {
-      // Adjust the seats per row for the last row
-      let seatsPerRow = i < totalSeats - lastRowSeats ? seatsInRow : lastRowSeats;
-
-      // Check if there are enough contiguous available seats in this row
-      if (
-        (i % seatsPerRow) + parseInt(seatCount) <= seatsPerRow &&
-        seats.slice(i, i + parseInt(seatCount)).every((x) => x === false)
-      ) {
-        // If so, book them
-        const newSeats = [...seats];
-        for (let j = 0; j < seatCount; j++) {
-          newSeats[i + j] = true; // mark seat as booked
-          bookedSeats.push(generateSeatNumber(i + j)); // store the seat number
-        }
-        setSeats(newSeats);
-        break; // exit the loop once we've booked the seats
-      }
+    if (seatCount === "" || parseInt(seatCount) <= 0) {
+      setAlert("Please enter a valid number of seats");
+      setTimeout(() => {
+        setAlert("");
+      }, 3000);
+      return;
     }
 
-    if (bookedSeats.length > 0) {
-      setBookedSeats(bookedSeats);
-    } else {
-        setAlert("Max 7 seats can be booked at a time");
+    fetch("http://localhost:8080/book", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ seatCount: parseInt(seatCount) }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.bookedSeats && data.bookedSeats.length > 0) {
+          const newSeats = [...seats];
+          data.bookedSeats.forEach((seatNumber) => {
+            const seatIndex = getSeatIndex(seatNumber);
+            if (seatIndex !== -1) {
+              newSeats[seatIndex] = true;
+            }
+          });
+          setSeats(newSeats);
+          setBookedSeats(data.bookedSeats);
+        } else {
+          setAlert("You can book 7 seats at a time");
+          setTimeout(() => {
+            setAlert("");
+          }, 3000);
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        setAlert("Something went wrong. Please try again later.");
         setTimeout(() => {
           setAlert("");
         }, 3000);
-    }
+      });
   };
 
   const generateSeatNumber = (seatIndex) => {
@@ -53,20 +90,29 @@ function SeatBooking() {
     return rowLetter + seatNumber;
   };
 
+  const getSeatIndex = (seatNumber) => {
+    for (let i = 0; i < totalSeats; i++) {
+      if (generateSeatNumber(i) === seatNumber) {
+        return i;
+      }
+    }
+    return -1;
+  };
+
   const handleChange = (event) => {
     setSeatCount(event.target.value);
   };
 
   return (
-    <div style={{ textAlign: "center",width:"80%",height:"100%",margin:"0px auto",boxShadow: "rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 1px 3px 1px",backgroundColor:"white",padding:"20px"}}>
-      <h1 style={{fontSize:"25px",margin:"20px"}}>Seat Booking System</h1>
+    <div style={{ textAlign: "center", width: "80%", height: "100%", margin: "0px auto", boxShadow: "rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 1px 3px 1px", backgroundColor: "white", padding: "20px" }}>
+      <h1 style={{ fontSize: "25px", margin: "20px" }}>Seat Booking System</h1>
       <p>Enter the number of seats you want to book:</p>
       <input
         type="number"
         value={seatCount}
         onChange={handleChange}
         placeholder="No of seats you want to book. eg. 1"
-        style={{ margin: "5px auto",width:"40%",height:"30px",border:"1px solid gray",borderTopLeftRadius:"20px",borderBottomLeftRadius:"20px",paddingLeft:"10px"}}
+        style={{ margin: "5px auto", width: "40%", height: "30px", border: "1px solid gray", borderTopLeftRadius: "20px", borderBottomLeftRadius: "20px", paddingLeft: "10px" }}
       />
       <button
         onClick={handleSeatBooking}
@@ -75,21 +121,21 @@ function SeatBooking() {
           backgroundColor: "#f44336",
           color: "white",
           border: "none",
-            borderTopRightRadius:"20px",
-            borderBottomRightRadius:"20px",
+          borderTopRightRadius: "20px",
+          borderBottomRightRadius: "20px",
           cursor: "pointer",
         }}
       >
         Book
       </button><br /><br />
-      <h3 style={{ color: "red",fontWeight:"400"}}>{Alert}</h3>
+      <h3 style={{ color: "red", fontWeight: "400" }}>{alert}</h3>
       <br /><br /><hr />
       {bookedSeats.length > 0 && (
-        <div style={{ marginTop: "10px",display:"flex",alignItems:"center"}}><strong>Booked Seat no.</strong><p style={{padding:"3px 10px",backgroundColor:"black",color:"white",borderRadius:"20px",}}>{" "}{bookedSeats.join(", ")}</p></div>
+        <div style={{ marginTop: "10px", display: "flex", alignItems: "center" }}><strong>Booked Seat no.</strong><p style={{ padding: "3px 10px", backgroundColor: "black", color: "white", borderRadius: "20px", }}>{" "}{bookedSeats.join(", ")}</p></div>
       )}
       <br />
-<hr />
-      <div style={{ marginTop: "20px",display:'grid',gridTemplateColumns:"repeat(7,1fr)" }}>
+      <hr />
+      <div style={{ marginTop: "20px", display: 'grid', gridTemplateColumns: "repeat(7,1fr)" }}>
         {seats.map((isBooked, index) => (
           <div
             key={index}
@@ -102,10 +148,9 @@ function SeatBooking() {
               borderRadius: "3px",
             }}
           >
-        {generateSeatNumber(index)}
+            {generateSeatNumber(index)}
           </div>
         ))}
-        
       </div>
     </div>
   );
